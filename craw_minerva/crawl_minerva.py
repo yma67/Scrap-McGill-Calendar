e421ac95ea4e8b014a05558f96706302fcc3b16c
yma67 <yuxiang.ma@mail.mcgill.ca>
2018-07-24 01:39:32 -0400

    more feature
    This update enables crawlers to get all sections, including lecture, exam, lab, and tutorials.

--- craw_minerva/crawl_minerva.py
+++ craw_minerva/crawl_minerva.py
@@ -5,6 +5,23 @@ import time, re
 from bs4 import BeautifulSoup
 import json
 
+
+def process1_name(namestr):
+    ent=list()
+    if namestr.find("TBA")!=-1:
+        return []
+    else:
+        matchObj=re.compile(r'[\,]').split(namestr)
+    for every in matchObj:
+        every=every.lstrip().rstrip()
+        name_component=re.compile(r'[\s]*').split(every)
+        name_prof=str()
+        for parts in name_component:
+            name_prof=name_prof+" "+parts
+        ent.append(name_prof.lstrip().rstrip())
+    return ent
+
+
 with open('username_pswd.txt') as f:
     lines = f.readlines()
 
@@ -23,21 +40,19 @@ driver.find_element_by_id("mcg_un_submit").click()
 driver.find_element_by_link_text("Student Menu").click()
 driver.find_element_by_link_text("Registration Menu").click()
 driver.find_element_by_link_text("Step 2: Search Class Schedule and Add Course Sections").click()
-Select(driver.find_element_by_id("term_input_id")).select_by_visible_text("Winter 2018")
-driver.find_element_by_xpath("//option[@value='201801']").click()
+driver.find_element_by_xpath("//option[@value='"+lines[3]+"']").click()
 driver.find_element_by_xpath("//input[@value='Submit']").click()
 
-"""
 # Select All department
 for subj in range(1, 242):
     driver.find_element_by_xpath("//*[@id='subj_id']/option["+str(subj)+"]").click()
 driver.find_element_by_xpath("/html/body/div[3]/form/input[17]").click()
-"""
-driver.find_element_by_xpath("//*[@id='subj_id']/option[83]").click()
-driver.find_element_by_xpath("/html/body/div[3]/form/input[17]").click()
+
 # for all course, parse information
 res=driver.find_elements(By.NAME, 'SUB_BTN')
 for course in range(1, len(res)+1):
+    course_title=driver.find_element_by_xpath("/html/body/div[3]/table[2]/tbody/tr["+str(2+course)+"]/td[1]").text
+    print(course_title)
     driver.find_element_by_xpath("(//input[@name='SUB_BTN'])["+str(course)+"]").click()
     bsobj=BeautifulSoup(driver.page_source)
     while bsobj.find("h1").get_text().find("No Response from Application Web Server")!=-1:
@@ -47,11 +62,12 @@ for course in range(1, len(res)+1):
 
     # for courses involve a lecture, crawl lecture, for courses involve a project, crawl project
     try:
-        sections = bsobj.find_all("td", text="Lecture")
+        sections = bsobj.find_all("td", text=course_title)
         if len(sections)==0:
             raise AttributeError
         sections = [sec.parent for sec in sections]
     except AttributeError:
+        print("l")
         sections=bsobj.find_all('td', {'class': 'dddefault'})
         sections=[sec.parent for sec in sections][:1]
     tabl=list()
@@ -65,11 +81,11 @@ for course in range(1, len(res)+1):
             full_rat = int(tabl[11]) / int(tabl[10])
         except ZeroDivisionError:
             full_rat = 0
-        lecs[tabl[4]]={'title': tabl[7], 'type': tabl[5], 'credit': tabl[6], 'days': tabl[8], 'time': tabl[9], 'capital': tabl[10], 'actual': tabl[11], 'full reg rate': str(full_rat), 'instructor': tabl[16], 'location': tabl[18]}
+        lecs[tabl[4]]={'title': tabl[7], 'type': tabl[5], 'credit': tabl[6], 'days': tabl[8], 'time': tabl[9], 'capital': tabl[10], 'actual': tabl[11], 'full reg rate': str(full_rat), 'instructor': process1_name(tabl[16]), 'location': tabl[18]}
 
     course_db[tabl[2] + " " + tabl[3]] = lecs
     driver.execute_script("window.history.go(-1)")
-    time.sleep(int(lines[2].lstrip().rstrip()))
+    time.sleep(1)
     continue
 print(course_db)
 
